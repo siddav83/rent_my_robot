abc413be6b8507d0ea0229bf6f2e40cf69f91757
DanielAwaritefe <awaritefe@gmail.com>
2021-02-23 11:04:43 +0000

    added controllers and view

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,7 +3,27 @@ class AppointmentsController < ApplicationController
     @appointments = Appointments.where(user.id == user_id)
   end
 
-  # def show
-  #   @appointments = Appointment.find(params[:robot_id])
-  # end
+  def new
+    @appointment = Appointment.new
+  end
+
+  def create
+    @appointment = Appointment.new(appointment_params)
+    @appointment.save
+  end
+
+  def show
+    @appointments = Appointment.find(params[:robot_id])
+  end
+
+  def delete
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:sessions_duration, :sessions_datetime)
+  end
 end
