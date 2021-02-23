6f1b4d8012cfe9fdb664e06094bffee4d641fc3b
Alec <68784184+W-edu@users.noreply.github.com>
2021-02-23 14:37:24 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :robots
+  resources :robots do
+    resources :appointments
   devise_for :users
   root to: 'pages#home'
   resources :users
-
-  end
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
