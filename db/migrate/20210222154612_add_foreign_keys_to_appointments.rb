0620cdcd532b39df88fecac7613ea79e82753a79
DanielAwaritefe <awaritefe@gmail.com>
2021-02-24 15:26:46 +0000

    nav footer and schema changes

--- db/migrate/20210222154612_add_foreign_keys_to_appointments.rb
+++ db/migrate/20210222154612_add_foreign_keys_to_appointments.rb
@@ -1,6 +1,6 @@
 class AddForeignKeysToAppointments < ActiveRecord::Migration[6.0]
   def change
-    add_reference :appointments, :robots, null: false, foreign_key: true
-    add_reference :appointments, :users, null: false, foreign_key: true
+    add_reference :appointments, :robot, null: false, foreign_key: true
+    add_reference :appointments, :user, null: false, foreign_key: true
   end
 end
